b47f84a83695afd97e6f52620029598758906fb8
Srta-Iny <iny.mercado@gmail.com>
2016-11-04 21:59:21 -0300

    trabajo terminado

--- js/main.js
+++ js/main.js
@@ -1,69 +1,62 @@
-window.addEventListener("load", init);
- 
-var todos = [];
- 
-function init()
-{
-	var add = document.querySelector("#form-submit");
-	add.addEventListener("click", addTodo);
+//realizar funcion que guarde las tareas indicadas en el caja mensajes
+// crear var para recorrer DOM
+// crear var para guardar respuesta
+//hay que confirmar si hay informacion en la caja de mansajes
+//guardar tarea y asignar un checkbox para dicha tarea
+
+var listaTareas = document.getElementById("tareas");
+var numTareas = document.getElementById("contador");
+var cont = 0;
+function anadirTarea(){
+//rescatar texto
+	var textMensaje = document.getElementById("mensaje").value;
+//nodo texto
+	var textTarea = document.createTextNode(textMensaje);
+//var elementos de las tareas
+	var cajaTarea = document.createElement("div");
+	var labelTarea = document.createElement("label");
+	var inputTarea = document.createElement("input");
+	var spanTarea = document.createElement("span");
+	var iconTrash = document.createElement("i");
+	var iconHeart = document.createElement("i");
+// añadir atributos
+ 	cajaTarea.setAttribute("class", "checkbox");
+	inputTarea.setAttribute("type", "checkbox");
+	iconTrash.setAttribute("class", "fa fa-trash icon-ba");
+	iconTrash.setAttribute("aria-hidden", "true");
+	iconHeart.setAttribute("class", "fa fa-heart icon-co");
+	iconHeart.setAttribute("aria-hidden", "true");
+// borrar, cambio de color y tachado
+	iconTrash.addEventListener("click", function(event){
+		eliminar(cajaTarea);
+	});
+	inputTarea.addEventListener("click", function(){
+		spanTarea.classList.toggle("tachado");
+	});
+	iconHeart.addEventListener("click", function(){
+		iconHeart.classList.toggle("rojo");
+	});
+//existe texto?
+	if(textMensaje === ""){
+		return false;
+	}else{
+	//sumar contador
+		cont++;
+		numTareas.innerHTML=cont;
+		labelTarea.appendChild(inputTarea);
+		spanTarea.appendChild(textTarea);
+		labelTarea.appendChild(spanTarea);
+		cajaTarea.appendChild(labelTarea);
+		cajaTarea.appendChild(iconTrash);
+		cajaTarea.appendChild(iconHeart);
+		listaTareas.appendChild(cajaTarea);
+	}
+// dejando espacio en blanco
+	textMensaje = document.getElementById("mensaje").value = "";
 }
- 
-function addTodo(event)
-{
-	//prevenimos el evento del enlace
-	event.preventDefault();
-	//comprobamos que tenga texto
-	if(document.querySelector("#mensaje").value === "") return;
- 
-	//creamos el objeto con la información del li
-	var li = 
-	{
-		index: todos.length + 1,
-		value: document.querySelector("#mensaje").value
-	};
-	todos.push(li);
-	displayTodos(li);
-}
- 
-function displayTodos(li)
-{
-	//creamos el elemento li
-    var node = document.createElement("li"); 
-    node.id = "mensaje"+li.index;
- 
-    //creamos el contenido del elementi li
-	var textnode = document.createTextNode(li.value);    
-	node.appendChild(textnode);   
-	document.querySelector("#tareas").appendChild(node); 
-	document.querySelector("#mensaje").value = "";
- 
-	//añadimos el botón
-	var deleteButton = createButton(li);
-    node.appendChild(deleteButton);
-}
- 
-function createButton(li)
-{
-	//creamos un input
-	var todo = document.createElement("input");
-	//de tipo button
-    todo.type = "button";
-    //añadimos un margen
-    todo.style.marginTop = "12px";
-    //flotamos a la derecha
-    todo.style.float = "right";
-    //el texto del input
-    todo.value = "Eliminar";
-    //añadimos un margen
-    todo.style.marginLeft = "15px";
-    //añadimos varias clases
-    todo.classList.add("removeTodo", "btn", "btn-danger");
-    //añadimos el evento click para que sea eliminado
-    todo.onclick = function() 
-    { 
-        var toDelete = document.querySelector('#mensaje'+li.index);
-		toDelete.parentNode.removeChild(toDelete);
-    };
-    //devolvemos el nuevo elemento
-    return todo;
+//funcion para borrar
+function eliminar(trash) {
+	cont--;
+	numTareas.innerHTML=cont;
+ 	trash.parentNode.removeChild(trash);
 }
